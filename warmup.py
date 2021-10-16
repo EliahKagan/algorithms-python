a4e36760eca2e45943a0fe5b7ccc3e4d9f5479b5
Eliah Kagan <degeneracypressure@gmail.com>
2021-10-15 21:29:21 -0400

    initial commit
    Mergesort is taking as long as insertion sort, even as sizes
    increase, so something is seriously wrong (bug).

---
+++ warmup.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+"""
+Implementations for warmup exercises in
+https://github.com/EliahKagan/algorithms-suggestions/blob/master/algorithms-suggestions.md
+(but adapted to Python).
+"""
+
+from timeit import timeit
+
+
+def put(values):
+    """
+    Prints values on a line, separated by commas, ended with a period.
+
+    >>> a = [10, 20, 30, 40, 50, 60, 70]
+    >>> put(a)
+    10, 20, 30, 40, 50, 60, 70.
+    >>> put(['foo'])
+    foo.
+    >>> put(())
+    .
+    """
+    print(*values, sep=', ', end='.\n')
+
+
+def timsort(values):
+    """
+    Sorts a list of values in place using Python's sort method for lists.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> b = a[:]
+    >>> timsort(a)
+    >>> a == sorted(b)
+    True
+    >>> put(a)
+    -5, 1, 1, 2, 3, 4, 6, 7, 8, 9.
+    """
+    values.sort()
+
+
+def sort_with_odds_first(nums):
+    """
+    Sorts a list of numbers in ascending order, but with all odds first.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> sort_with_odds_first(a)
+    >>> a
+    [-5, 1, 1, 3, 7, 9, 2, 4, 6, 8]
+    """
+    nums.sort(key=lambda num: (num % 2 == 0, num))
+
+
+def find_index(values, key):
+    """
+    Does sequential search in values for key. Returns an index where the key
+    appears, or None if the sequences of values doesn't contain the key.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> find_index(a, 3)
+    8
+    >>> find_index(a, 7)
+    1
+    >>> find_index(a, 5)
+    >>>
+    """
+    try:
+        return values.index(key)
+    except ValueError:
+        return None
+
+
+def insertion_sort(values, key=None):
+    """
+    Sorts values in place by insertion sort, using the key selector if given.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> insertion_sort(a)
+    >>> a
+    [-5, 1, 1, 2, 3, 4, 6, 7, 8, 9]
+    >>> insertion_sort(a, key=lambda x: -x)
+    >>> a
+    [9, 8, 7, 6, 4, 3, 2, 1, 1, -5]
+    >>> insertion_sort(a, key=abs)
+    >>> a
+    [1, 1, 2, 3, 4, -5, 6, 7, 8, 9]
+    """
+    if key is None:
+        key = lambda x: x
+
+    for right in range(1, len(values)):
+        for left in range(right, 0, -1):
+            if key(values[left - 1]) <= key(values[left]):
+                break
+            values[left - 1], values[left] = values[left], values[left - 1]
+
+
+def _merge(values, low, mid, high, aux, key):
+    """
+    Merges values[low:mid] and values[mid:high] back into values[:], using
+    aux as auxiliary storage. Each range is assumed to be sorted relative to
+    < with the specified key selector.
+
+    This is a helper function for mergesort and mergesort_bottomup.
+    """
+    assert not aux, 'The auxiliary storage is expected to start empty.'
+
+    left = low
+    right = mid
+
+    while left < mid and right < high:
+        if key(values[right]) < key(values[left]):
+            aux.append(values[right])
+            right += 1
+        else:
+            aux.append(values[left])
+            left += 1
+
+    aux.extend(values[left:mid])
+    assert len(aux) == right - low, 'Wrong number of values copied.'
+    values[low:right] = aux
+    aux.clear()
+
+
+def mergesort(values, key=None):
+    """
+    Sorts values in place by recursive top-down mergesort, using the key
+    selector if given.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> mergesort(a)
+    >>> a
+    [-5, 1, 1, 2, 3, 4, 6, 7, 8, 9]
+    >>> mergesort(a, key=lambda x: -x)
+    >>> a
+    [9, 8, 7, 6, 4, 3, 2, 1, 1, -5]
+    >>> mergesort(a, key=abs)
+    >>> a
+    [1, 1, 2, 3, 4, -5, 6, 7, 8, 9]
+    """
+    if key is None:
+        key = lambda x: x
+    aux = []
+
+    def mergesort_sublist(low, high):
+        delta = high - low
+        if delta < 2:
+            return
+        mid = low + delta // 2
+        mergesort_sublist(low, mid)
+        mergesort_sublist(mid, high)
+        _merge(values, low, mid, high, aux, key)
+
+    mergesort_sublist(0, len(values))
+
+
+def mergesort_bottomup(values, key=None):
+    """
+    Sorts values in place by iterative bottom-up mergesort, using the key
+    selector if given.
+
+    >>> a = [1, 7, 4, 9, 2, 6, 1, 8, 3, -5]
+    >>> mergesort_bottomup(a)
+    >>> a
+    [-5, 1, 1, 2, 3, 4, 6, 7, 8, 9]
+    >>> mergesort_bottomup(a, key=lambda x: -x)
+    >>> a
+    [9, 8, 7, 6, 4, 3, 2, 1, 1, -5]
+    >>> mergesort_bottomup(a, key=abs)
+    >>> a
+    [1, 1, 2, 3, 4, -5, 6, 7, 8, 9]
+    """
+    if key is None:
+        key = lambda x: x
+    aux = []
+
+    delta = 1
+    while delta < len(values):
+        for low in range(0, len(values) - delta, delta * 2):
+            mid = low + delta
+            high = min(mid + delta, len(values))
+            _merge(values, low, mid, high, aux, key)
+        delta *= 2
+
+
+def benchmark_sorts(values):
+    """
+    Benchmarks all three sorting algorithms implemented here, by sorting copies
+    of values with each of them.
+    """
+    sorted_values = sorted(values)
+    quantity = '1 value' if len(values) == 1 else f'{len(values)} values'
+
+    for sorter in (insertion_sort, mergesort, mergesort_bottomup):
+        copied_values = values[:]
+        duration = timeit(lambda: insertion_sort(copied_values), number=1)
+        if copied_values != sorted_values:
+            raise AssertionError('Sorting failed!')
+        print(f'On {quantity}, {sorter.__name__} took {duration * 1000} ms.')
+
+
+__all__ = [thing.__name__ for thing in (
+    put,
+    timsort,
+    sort_with_odds_first,
+    find_index,
+    insertion_sort,
+    mergesort,
+    mergesort_bottomup,
+    benchmark_sorts,
+)]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
